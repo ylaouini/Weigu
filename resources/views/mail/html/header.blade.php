2892a1c36529029937a8743daa33d8d25cb6cd73
Yassine LAOUINI <laouini.info@gmail.com>
2021-07-29 09:22:51 +0100

    add email view

---
+++ resources/views/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+<tr>
+    <td class="header">
+        <div class="mail-logo">
+{{--            <img src="https://i.imgur.com/7oCx8kL.png" alt="{{ config('app.name') }} Logo">--}}
+            <h1 class="pagelogo">Weigu</h1>
+        </div>
+    </td>
+</tr>
