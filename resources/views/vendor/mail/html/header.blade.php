b75f9670e2e984a4d1972e3ea48dba1e12c7d325
Yassine LAOUINI <laouini.info@gmail.com>
2021-07-30 16:39:40 +0100

    add logo image to email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,7 @@
 <tr>
     <td class="header">
         <div class="mail-logo">
-{{--            <img src="https://i.imgur.com/7oCx8kL.png" alt="{{ config('app.name') }} Logo">--}}
-            <h1 class="pagelogo">Weigu</h1>
+            <img src="{{ URL::asset('images/WeiguLogo.png') }}"  alt="{{ config('app.name') }} Logo">
         </div>
     </td>
 </tr>
