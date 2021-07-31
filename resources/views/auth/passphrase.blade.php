e16e0b2ee5f3c138a7476169b6fa7c86195da5ff
Yassine LAOUINI <laouini.info@gmail.com>
2021-07-31 16:26:45 +0100

    update style

--- resources/views/auth/passphrase.blade.php
+++ resources/views/auth/passphrase.blade.php
@@ -1,29 +1,33 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <!-- <x-jet-authentication-card-logo /> -->
+            <h1 class="pagelogo">Weigu</h1>
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
 
         @if (session('status'))
-            <div class="mb-4 text-sm font-medium text-green-600">
-                {{ session('status') }}
-            </div>
+        <div class="mb-4 text-sm font-medium text-green-600">
+            {{ session('status') }}
+        </div>
         @endif
 
         <form method="POST" action="{{ route('login.confirmation') }}">
             @csrf
 
             <div>
-                <x-jet-label value="Pass-phrase" for="passphrase" />
-                <x-jet-input class="block w-full mt-1" type="text" name="passphrase" :value="old('passphrase')" required autofocus />
+                <x-jet-label class="mb-4" value="Lorem lorem lorem" for="passphrase" />
+                <div class="group">
+                    <input class="my-inputs" class="" type="text" name="passphrase" required autofocus>
+                    <span class="highlight"></span>
+                    <span class="bar"></span>
+                    <label>Passphrase</label>
+                </div>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <x-jet-button class="ml-4">
-                    Confirm
-                </x-jet-button>
+            <div class="flex items-center justify-center mt-4">
+                <input type="submit" class="my-buttons" value="CONFIRMER">
             </div>
         </form>
     </x-jet-authentication-card>
