3d53934c6afa5e8bc55eeb59d1165e3e61efba10
Yassine LAOUINI <laouini.info@gmail.com>
2021-08-07 20:07:12 +0100

    change the greeting in mails

--- app/Notifications/BroadcastMessage.php
+++ app/Notifications/BroadcastMessage.php
@@ -52,7 +52,7 @@ class BroadcastMessage extends Notification
         return (new MailMessage)
             ->from('noreply@weigu-app.com')
             ->subject('Nouveau message')
-            ->line('Salut '.$this->receiver->name)
+            ->greeting('Salut '.$this->receiver->name)
             ->line('Vous avez reçu un nouveau message de '.$this->sender.':')
             ->line($this->message->message)
             ->action('Aller au message', URL::route('chat'))
