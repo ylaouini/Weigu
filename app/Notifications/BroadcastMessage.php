6c4482af8f527b76950b8b9b329c7435ac009184
Yassine LAOUINI <laouini.info@gmail.com>
2021-08-07 16:09:29 +0100

    creat a new notification broadcastMessage

---
+++ app/Notifications/BroadcastMessage.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\URL;
+
+class BroadcastMessage extends Notification
+{
+    use Queueable;
+
+    public $receiver;
+    public $sender;
+    public $message;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(User $receiver, string $sender, \App\Models\BroadcastMessage $message)
+    {
+        $this->receiver = $receiver;
+        $this->sender = $sender;
+        $this->message = $message;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param mixed $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->from('noreply@weigu-app.com')
+            ->subject('Nouveau message')
+            ->line('Salut '.$this->receiver->name)
+            ->line('Vous avez reçu un nouveau message de '.$this->sender.':')
+            ->line($this->message->message)
+            ->action('Aller au message', URL::route('chat'))
+            ->line('Merci d\'utiliser notre application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
