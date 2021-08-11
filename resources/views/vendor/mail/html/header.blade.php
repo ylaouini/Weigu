0cc4c083ff5580f7292512a9eda121e2fb69bccf
Yassine LAOUINI <laouini.info@gmail.com>
2021-08-11 05:30:01 +0100

    publish email view

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,11 @@
 <tr>
-    <td class="header">
-        <div class="mail-logo">
-            <img src="{{ URL::asset('images/WeiguLogo.png') }}"  alt="{{ config('app.name') }} Logo">
-        </div>
-    </td>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+@if (trim($slot) === 'Laravel')
+        <img src="{{ URL::asset('images/WeiguLogo.png') }}"  alt="{{ config('app.name') }} Logo">
+@else
+{{ $slot }}
+@endif
+</a>
+</td>
 </tr>
