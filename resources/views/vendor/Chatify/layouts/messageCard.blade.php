935524cf04f35868a377e1c89c1a58b15c2da9a1
Yassine LAOUINI <laouini.info@gmail.com>
2021-08-12 07:25:30 +0100

    add style to question

--- resources/views/vendor/Chatify/layouts/messageCard.blade.php
+++ resources/views/vendor/Chatify/layouts/messageCard.blade.php
@@ -1,25 +1,27 @@
 {{-- -------------------- The default card (white) -------------------- --}}
 @if($viewType == 'default')
     @if($from_id != $to_id)
-    <div class="message-card" data-id="{{ $id }}">
-        <p>{!! ($message == null && $attachment != null && @$attachment[2] != 'file') ? $attachment[1] : nl2br($message) !!}
-            <sub title="{{ $fullTime }}">{{ $time }}</sub>
-            {{-- If attachment is a file --}}
-            @if(@$attachment[2] == 'file')
-            <a href="{{ route(config('chatify.attachments.download_route_name'),['fileName'=>$attachment[0]]) }}" style="color: #595959;" class="file-download">
-                <span class="fas fa-file"></span> {{$attachment[1]}}</a>
-            @endif
-        </p>
-    </div>
-    {{-- If attachment is an image --}}
-    @if(@$attachment[2] == 'image')
-    <div>
-        <div class="message-card">
-            <div class="image-file chat-image" style="width: 250px; height: 150px;background-image: url('{{ asset('storage/'.config('chatify.attachments.folder').'/'.$attachment[0]) }}')">
-            </div>
+        <div class="message-card" data-id="{{ $id }}">
+                <p @if($broadcast_message_id != null) style="background: #00b050;color: #fff" @endif>{!! ($message == null && $attachment != null && @$attachment[2] != 'file') ? $attachment[1] : nl2br($message) !!}
+                    <sub title="{{ $fullTime }}">{{ $time }}</sub>
+                    {{-- If attachment is a file --}}
+                    @if(@$attachment[2] == 'file')
+                        <a href="{{ route(config('chatify.attachments.download_route_name'),['fileName'=>$attachment[0]]) }}"
+                           style="color: #595959;" class="file-download">
+                            <span class="fas fa-file"></span> {{$attachment[1]}}</a>
+                    @endif
+                </p>
         </div>
-    </div>
-    @endif
+        {{-- If attachment is an image --}}
+        @if(@$attachment[2] == 'image')
+            <div>
+                <div class="message-card">
+                    <div class="image-file chat-image"
+                         style="width: 250px; height: 150px;background-image: url('{{ asset('storage/'.config('chatify.attachments.folder').'/'.$attachment[0]) }}')">
+                    </div>
+                </div>
+            </div>
+        @endif
     @endif
 @endif
 
@@ -29,20 +31,22 @@
         <p>{!! ($message == null && $attachment != null && @$attachment[2] != 'file') ? $attachment[1] : nl2br($message) !!}
             <sub title="{{ $fullTime }}" class="message-time">
                 <span class="fas fa-{{ $seen > 0 ? 'check-double' : 'check' }} seen"></span> {{ $time }}</sub>
-                {{-- If attachment is a file --}}
+            {{-- If attachment is a file --}}
             @if(@$attachment[2] == 'file')
-            <a href="{{ route(config('chatify.attachments.download_route_name'),['fileName'=>$attachment[0]]) }}" class="file-download">
-                <span class="fas fa-file"></span> {{$attachment[1]}}</a>
+                <a href="{{ route(config('chatify.attachments.download_route_name'),['fileName'=>$attachment[0]]) }}"
+                   class="file-download">
+                    <span class="fas fa-file"></span> {{$attachment[1]}}</a>
             @endif
         </p>
     </div>
     {{-- If attachment is an image --}}
     @if(@$attachment[2] == 'image')
-    <div>
-        <div class="message-card mc-sender">
-            <div class="image-file chat-image" style="width: 250px; height: 150px;background-image: url('{{ asset('storage/'.config('chatify.attachments.folder').'/'.$attachment[0]) }}')">
+        <div>
+            <div class="message-card mc-sender">
+                <div class="image-file chat-image"
+                     style="width: 250px; height: 150px;background-image: url('{{ asset('storage/'.config('chatify.attachments.folder').'/'.$attachment[0]) }}')">
+                </div>
             </div>
         </div>
-    </div>
     @endif
 @endif
