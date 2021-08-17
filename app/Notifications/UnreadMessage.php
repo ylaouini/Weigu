8e0e33c15f7cb5cab07e3ca94c4202cab162469c
Yassine LAOUINI <laouini.info@gmail.com>
2021-08-17 16:53:29 +0100

    email last message only

--- app/Notifications/UnreadMessage.php
+++ app/Notifications/UnreadMessage.php
@@ -22,7 +22,6 @@ class UnreadMessage extends Notification implements ShouldQueue
     public function __construct($messageCount)
     {
         $this->messageCount = $messageCount;
-
     }
 
     /**
@@ -46,6 +45,7 @@ class UnreadMessage extends Notification implements ShouldQueue
     {
         $url = URL::route('chat');
         $messages = ChMessage::where('to_id',$notifiable->id)->where('seen',0)->where('email_sent',0)->get();
+        $messages = $messages->unique('to_id');
         return (new MailMessage)
             ->subject('Nouveau message privé')
             ->from('noreply@weigu-app.com','Weigu')
@@ -55,13 +55,6 @@ class UnreadMessage extends Notification implements ShouldQueue
                 'url' => $url,
                 'messages' => $messages
             ]);
-//        return (new MailMessage)
-//            ->from('noreply@weigu-app.com','Weigu')
-//            ->subject('Nouveau message privé')
-//            ->greeting('Salut '.$notifiable->name)
-//            ->line('Tu as '.$this->messageCount.' message(s) non lu')
-//            ->action('Aller au message', URL::route('chat'))
-//            ->line('Merci d\'utiliser notre application!');
     }
 
     /**
